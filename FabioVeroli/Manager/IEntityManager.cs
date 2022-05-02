36bb2ac94e3e80151f391b6a5036bb9ea0b3b2f6
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-05-02 18:36:53 +0200

    Create interface for entities' manager

---
+++ FabioVeroli/Manager/IEntityManager.cs
@@ -0,0 +1,28 @@
+﻿using FabioVeroli.Entity;
+using System.Collections.Generic;
+
+namespace FabioVeroli.Manager
+{
+    /// <summary>
+    /// Interface identifying the manager for IDynamicEntity.
+    /// </summary>
+    public interface IEntityManager
+    {
+        /// <summary>
+        /// Get the entities currently on the game. 
+        /// </summary>
+        /// <returns>a list containing the entities that are actually on the game.</returns>
+        List<IDynamicEntity> GetEntities();
+
+        /// <summary>
+        /// Speed property of the entities. 
+        /// </summary>
+        float SpeedX { set; }
+
+        /// <summary>
+        /// Update the position of every entity in the list, remove and add element.
+        /// </summary>
+        void UpdateList();
+
+    }
+}
