b3b7c0e853c6fbe26255fb89be2806b7d3fefb10
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-05-02 14:30:28 +0200

    Create ShopModel interface

---
+++ RacheleMargutti/Shop/IShopModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RacheleMargutti.Shop
+{
+    interface IShopModel
+    {
+        void ShopItemPayment(ShopItem selectedItem);
+
+        string MysteryBoxPayment();
+
+        int TotalCoins { get; }
+
+        List<ShopItem> Items { get; }
+
+        List<ShopItem> PurchasedItems { get; }
+
+        void SaveShopItem();
+
+        bool CheckMystery(MysteryBox box, int coins);
+
+        bool CheckPayment(ShopItem selectedItem, int coins);
+
+        void WriteSkinsOnFile();
+
+        bool IsSelected(string name);
+
+        void SelectSkin(string name);
+    }
+}
