90f0ce16a17c9b6bb47174af7e24483344781ade
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-05-02 09:37:11 +0200

    Create interface defining an entities factory

---
+++ FabioVeroli/Factory/IEntityFactory.cs
@@ -0,0 +1,16 @@
+﻿using FabioVeroli.Entity;
+using System.Collections.Generic;
+
+namespace FabioVeroli.Factory
+{
+    interface IEntityFactory
+    {
+            IDynamicEntity CreateObstacle(SpawnLevel level);
+            IDynamicEntity CreatePlatform(SpawnLevel level);
+            IDynamicEntity CreateCoin(SpawnLevel level);
+            List<IDynamicEntity> CombinePlatformObstacle(SpawnLevel platformLevel, SpawnLevel obstacleLevel);
+            List<IDynamicEntity> CombinePlatformCoin(SpawnLevel platformLevel, SpawnLevel coinLevel);
+            List<IDynamicEntity> CombineObstacleCoin(SpawnLevel obstacleLevel, SpawnLevel coinLevel);
+            List<IDynamicEntity> CombineAll(SpawnLevel platformLevel, SpawnLevel obstacleLevel, SpawnLevel coinLevel);
+    }
+}
