c190534c1dc3f66b466b0b375fdf0cb7b7f3d45e
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-05-02 10:04:55 +0200

    Add comments

--- FabioVeroli/Entity/IDynamicEntity.cs
+++ FabioVeroli/Entity/IDynamicEntity.cs
@@ -2,23 +2,48 @@
 
 namespace FabioVeroli.Entity
 {
+    /// <summary>
+    /// Interface that describe an entity moving on the screen.
+    /// </summary>
     interface IDynamicEntity
     {
-
+        /// <summary>
+        /// Update the entity position on the screen.
+        /// </summary>
+        /// <param name="speedX"> entity's speedX. </param>
         void UpdatePosition(float speedX);
 
+        /// <summary>
+        /// Returns the entity's Bounding Box.
+        /// </summary>
+        /// <returns> a new RectangleF representing the coordinates and the dimension of the entity.</returns>
         RectangleF GetBounds();
 
+        /// <summary>
+        /// Check if the entity is out of the game screen.
+        /// </summary>
+        /// <returns>true if the entity is out of the screen, false otherwise.</returns>
         bool IsOutofScreen();
 
+        /// <summary>
+        /// The level on which the entity spawn.
+        /// </summary>
         SpawnLevel Level { get; }
 
+        /// <summary>
+        /// The type that identifies the entity.
+        /// </summary>
         EntityType Type { get; }
 
+        /// <summary>
+        /// The distance after that next entity should spawn.
+        /// </summary>
         double Distance { get;  }
 
+        /// <summary>
+        /// An entity' state, true if it was hit, false otherwise.
+        /// </summary>
         bool Hit { get; set; }
 
-        //void onCollision(Model model);
     }
 }
