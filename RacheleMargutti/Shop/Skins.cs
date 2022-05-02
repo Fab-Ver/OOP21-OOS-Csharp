aabe024e381ca600cf778437fd2a25fb47dd3d62
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-05-02 14:32:18 +0200

    Create a Skins class to substitute Java enum

---
+++ RacheleMargutti/Shop/Skins.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RacheleMargutti.Shop
+{
+    class Skins
+    {
+        public static readonly Skins PLAYER = new Skins("Player.png", 0);
+        public static readonly Skins PROGRAMMER = new Skins("Programmer.png", 1000);
+        public static readonly Skins DINOSAUR = new Skins("Dinosaur.png", 2000);
+
+        public static IEnumerable<Skins> Values { get; } = new[] { PLAYER, PROGRAMMER, DINOSAUR };
+
+        private Skins(string skinName, int price)
+        {
+            SkinName = skinName;
+            Price = price;
+        }
+
+        public string SkinName { get; }
+
+        public int Price { get; }
+ 
+    }
+}
