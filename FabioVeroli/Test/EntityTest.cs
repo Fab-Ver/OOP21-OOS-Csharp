7d73f6e656d9aebe7b7224eaa3672184abcad846
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-05-02 09:39:46 +0200

    Create tests for entity and entities' factory, tests work

---
+++ FabioVeroli/Test/EntityTest.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+using FabioVeroli.Entity;
+using FabioVeroli.Factory;
+using System.Drawing;
+
+namespace FabioVeroli.Test
+{   
+    class EntityTest
+    {
+        private static readonly float COIN_WIDTH = 35.0f;
+        private static readonly float COIN_HEIGHT = 40.0f;
+        private static readonly float COIN_EXPECTED_X = 1039.5f;
+        private static readonly float COIN_EXPECTED_Y = 290.0f;
+        private static readonly float COIN_EXPECTED_DISTANCE = 668.5f;
+
+        private static readonly float PLATFORM_WIDTH = 417.0f;
+        private static readonly float PLATFORM_HEIGHT = 35.0f;
+        private static readonly float PLATFORM_EXPECTED_X = 854.0f;
+        private static readonly float PLATFORM_EXPECTED_Y = 185.0f;
+        private static readonly float PLATFORM_EXPECTED_DISTANCE = 437.0f;
+
+        private static readonly float OBSTACLE_WIDTH = 43.0f;
+        private static readonly float OBSTACLE_HEIGHT = 55.0f;
+        private static readonly float OBSTACLE_EXPECTED_X = 1056.1f;
+        private static readonly float OBSTACLE_EXPECTED_Y = 385.0f;
+        private static readonly float OBSTACLE_EXPECTED_DISTANCE = 651.9f;
+
+        private static readonly float WORLD_WIDTH = 854.0f;
+        private static readonly float WORLD_HEIGHT = 440.0f;
+
+
+        private EntityFactory factory;
+
+        [SetUp]
+        public void SetUp() => this.factory = new EntityFactory(new SizeF(WORLD_WIDTH, WORLD_HEIGHT));
+
+        [Test]
+        public void TestCoin()
+        {
+            /*Test coin's initial state and position*/
+            IDynamicEntity coin = factory.CreateCoin(SpawnLevel.ONE);
+            Assert.AreEqual(coin.GetBounds(), new RectangleF(COIN_EXPECTED_X, COIN_EXPECTED_Y, COIN_WIDTH, COIN_HEIGHT));
+            Assert.IsFalse(coin.IsOutofScreen());
+            Assert.AreEqual(SpawnLevel.ONE, coin.Level);
+            Assert.AreEqual(EntityType.COIN, coin.Type);
+            Assert.AreEqual(COIN_EXPECTED_DISTANCE, coin.Distance);
+            Assert.IsFalse(coin.Hit);
+            /*Modify coin's state*/
+            coin.Hit = true;
+            coin.UpdatePosition(WORLD_WIDTH * 2);
+            /*Check the state modification*/
+            Assert.IsTrue(coin.Hit);
+            Assert.IsTrue(coin.IsOutofScreen());
+        }
+
+        [Test]
+        public void TestPlatform()
+        {
+            /*Test platform's initial state and position*/
+            IDynamicEntity platform = factory.CreatePlatform(SpawnLevel.TWO);
+            Assert.AreEqual(platform.GetBounds(), new RectangleF(PLATFORM_EXPECTED_X, PLATFORM_EXPECTED_Y, PLATFORM_WIDTH, PLATFORM_HEIGHT));
+            Assert.IsFalse(platform.IsOutofScreen());
+            Assert.AreEqual(SpawnLevel.TWO, platform.Level);
+            Assert.AreEqual(EntityType.PLATFORM, platform.Type);
+            Assert.AreEqual(PLATFORM_EXPECTED_DISTANCE, platform.Distance);
+            Assert.IsFalse(platform.Hit);
+            /*Modify platform's state*/
+            platform.Hit = true;
+            platform.UpdatePosition(WORLD_WIDTH * 2);
+            /*Check the state modification*/
+            Assert.IsTrue(platform.Hit);
+            Assert.IsTrue(platform.IsOutofScreen());
+        }
+
+        [Test]
+        public void TestObstacle()
+        {
+            /*Test obstacle's initial state and position*/
+            IDynamicEntity obstacle = factory.CreateObstacle(SpawnLevel.ZERO);
+
+            Assert.AreEqual(obstacle.GetBounds(), new RectangleF(OBSTACLE_EXPECTED_X, OBSTACLE_EXPECTED_Y, OBSTACLE_WIDTH, OBSTACLE_HEIGHT));
+            Assert.AreEqual(obstacle.Distance, OBSTACLE_EXPECTED_DISTANCE);
+            Assert.IsFalse(obstacle.IsOutofScreen());
+            Assert.AreEqual(SpawnLevel.ZERO, obstacle.Level);
+            Assert.AreEqual(EntityType.OBSTACLE, obstacle.Type);
+            Assert.IsFalse(obstacle.Hit);
+            /*Modify obstacle's state*/
+            obstacle.Hit = true;
+            obstacle.UpdatePosition(WORLD_WIDTH * 2);
+            /*Check the state modification*/
+            Assert.IsTrue(obstacle.Hit);
+            Assert.IsTrue(obstacle.IsOutofScreen());
+        }
+    }
+}
