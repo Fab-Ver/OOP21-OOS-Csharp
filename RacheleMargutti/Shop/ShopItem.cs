00efff6b247816fbe065f2a4b99c418a6b59bb83
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-05-02 14:31:09 +0200

    Implement ShopItem object

---
+++ RacheleMargutti/Shop/ShopItem.cs
@@ -0,0 +1,19 @@
+﻿namespace RacheleMargutti.Shop
+{
+    class ShopItem : IShopItem
+    {
+        public ShopItem(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; }
+
+        public int Price { get; }
+
+        public void Purchase() => IsPurchased = true;
+
+        public bool IsPurchased { get; private set; } = false;
+    }
+}
