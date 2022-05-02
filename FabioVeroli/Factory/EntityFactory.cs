1ddfdf48fe32a1f3b127aefa4a6ab28a5cb3cb16
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-05-02 17:21:27 +0200

    Implemented combine methods and refactor using commons.gemoetry classes created by us

--- FabioVeroli/Factory/EntityFactory.cs
+++ FabioVeroli/Factory/EntityFactory.cs
@@ -1,7 +1,6 @@
 ﻿using FabioVeroli.Entity;
-using System;
 using System.Collections.Generic;
-using System.Drawing;
+using Commons.Geometry;
 
 namespace FabioVeroli.Factory
 {
@@ -16,63 +15,55 @@ namespace FabioVeroli.Factory
         private static readonly float OBSTACLE_WIDTH = 43.0f;
         private static readonly float OBSTACLE_HEIGHT = 55.0f;
 
-        public EntityFactory(SizeF worldDimensions)
+        public EntityFactory(Size worldDimensions)
         {
             WorldDimensions = worldDimensions;
         }
 
-        public SizeF WorldDimensions { private get; set; }
+        public Size WorldDimensions { private get; set; }
 
         public IDynamicEntity CreateCoin(SpawnLevel level)
         {
-            SizeF size = new SizeF(COIN_WIDTH, COIN_HEIGHT);
-            PointF coordinates = GeneratePoint(level, size, EntityType.COIN.GetDistanceFactor());
+            Size size = new Size(COIN_WIDTH, COIN_HEIGHT);
+            Point coordinates = GeneratePoint(level, size, EntityType.COIN.GetDistanceFactor());
             float distance = WorldDimensions.Width - size.Width * EntityType.COIN.GetDistanceFactor();
             return new Platform(coordinates, size, level, EntityType.COIN, distance);
         }
 
         public IDynamicEntity CreateObstacle(SpawnLevel level)
         {
-            SizeF size = new SizeF(OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
-            PointF coordinates = GeneratePoint(level, size, EntityType.OBSTACLE.GetDistanceFactor());
+            Size size = new Size(OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
+            Point coordinates = GeneratePoint(level, size, EntityType.OBSTACLE.GetDistanceFactor());
             float distance = WorldDimensions.Width - size.Width * EntityType.OBSTACLE.GetDistanceFactor();
             return new Obstacle(coordinates, size, level, EntityType.OBSTACLE, distance);
         }
 
         public IDynamicEntity CreatePlatform(SpawnLevel level)
         {
-            SizeF size = new SizeF(PLATFORM_WIDTH, PLATFORM_HEIGHT);
-            PointF coordinates = GeneratePoint(level, size, EntityType.PLATFORM.GetDistanceFactor());
+            Size size = new Size(PLATFORM_WIDTH, PLATFORM_HEIGHT);
+            Point coordinates = GeneratePoint(level, size, EntityType.PLATFORM.GetDistanceFactor());
             float distance = WorldDimensions.Width - size.Width;
             return new Platform(coordinates, size, level, EntityType.PLATFORM, distance);
         }
 
-        public List<IDynamicEntity> CombineAll(SpawnLevel platformLevel, SpawnLevel obstacleLevel, SpawnLevel coinLevel)
-        {
-            throw new NotImplementedException();
-        }
+        public List<IDynamicEntity> CombineAll(SpawnLevel platformLevel, SpawnLevel obstacleLevel, SpawnLevel coinLevel) =>
+            new List<IDynamicEntity> { CreateCoin(coinLevel), CreateObstacle(obstacleLevel), CreatePlatform(platformLevel) };
 
-        public List<IDynamicEntity> CombineObstacleCoin(SpawnLevel obstacleLevel, SpawnLevel coinLevel)
-        {
-            throw new NotImplementedException();
-        }
+        public List<IDynamicEntity> CombineObstacleCoin(SpawnLevel obstacleLevel, SpawnLevel coinLevel) =>
+            new List<IDynamicEntity> { CreateCoin(coinLevel), CreateObstacle(obstacleLevel) };
 
-        public List<IDynamicEntity> CombinePlatformCoin(SpawnLevel platformLevel, SpawnLevel coinLevel)
-        {
-            throw new NotImplementedException();
-        }
+        public List<IDynamicEntity> CombinePlatformCoin(SpawnLevel platformLevel, SpawnLevel coinLevel) =>
+            new List<IDynamicEntity> { CreateCoin(coinLevel), CreatePlatform(platformLevel) };
 
-        public List<IDynamicEntity> CombinePlatformObstacle(SpawnLevel platformLevel, SpawnLevel obstacleLevel)
-        {
-            throw new NotImplementedException();
-        }
+        public List<IDynamicEntity> CombinePlatformObstacle(SpawnLevel platformLevel, SpawnLevel obstacleLevel) =>
+            new List<IDynamicEntity> { CreateObstacle(obstacleLevel), CreatePlatform(platformLevel) };
 
 
-        private PointF GeneratePoint(SpawnLevel level, SizeF size, float distanceFactor)
+        private Point GeneratePoint(SpawnLevel level, Size size, float distanceFactor)
         {
             float x = WorldDimensions.Width + size.Width * distanceFactor;
             float y = WorldDimensions.Height * level.GetSpawnY() - size.Height;
-            return new PointF(x, y);
+            return new Point(x, y);
         }
     }
 }
