1cdde74519ea7a72241b2f2290b6c06f4533aaaa
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-05-02 14:30:03 +0200

    Create ShopItem interface

---
+++ RacheleMargutti/Shop/IShopItem.cs
@@ -0,0 +1,11 @@
+﻿namespace RacheleMargutti.Shop
+{
+    interface IShopItem
+    {
+        bool IsPurchased { get; }
+        string Name { get; }
+        int Price { get; }
+
+        void Purchase();
+    }
+}
