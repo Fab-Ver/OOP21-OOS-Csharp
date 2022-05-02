7b9abad6e27bc0f0a2bc2cee1f05867a1162a4de
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-05-02 09:33:34 +0200

    Create entities interface

---
+++ FabioVeroli/Entity/IDynamicEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace FabioVeroli.Entity
+{
+    interface IDynamicEntity
+    {
+
+        void UpdatePosition(float speedX);
+
+        RectangleF GetBounds();
+
+        bool IsOutofScreen();
+
+        SpawnLevel Level { get; }
+
+        EntityType Type { get; }
+
+        double Distance { get;  }
+
+        bool Hit { get; set; }
+
+        //void onCollision(Model model);
+    }
+}
