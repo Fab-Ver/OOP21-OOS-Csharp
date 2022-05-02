5f7093790e806fa62ec245c332e974bbc049c89e
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-05-02 17:18:18 +0200

    Implmented class size and point used in some personal project

---
+++ Commons/Geometry/Size.cs
@@ -0,0 +1,14 @@
+﻿namespace Commons.Geometry
+{
+    public class Size
+    {
+        public Size(float width, float height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public float Width { get; set; }
+        public float Height { get; set; }
+    }
+}
