63411de0573c1be28abd572ae34ea609f1a3b1cc
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-05-04 15:57:06 +0200

    Fix constant name

--- Commons/Geometry/Image.cs
+++ Commons/Geometry/Image.cs
@@ -5,15 +5,15 @@
     /// </summary>
     public sealed class Image
     {
-        private const float testDim = 50.0f;
+        private const float TestDim = 50.0f;
 
         /// <summary>
         /// Create a new Image
         /// </summary>
         public Image()
         {
-            Width = testDim;
-            Height = testDim;
+            Width = TestDim;
+            Height = TestDim;
         }
 
         public float Width { get; }
