dc95c97df0912716090bbbe32b4b0c335741645b
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-05-04 17:30:07 +0200

    Fix fields name

--- DavideTonelli/Test/MarkerTest.cs
+++ DavideTonelli/Test/MarkerTest.cs
@@ -15,22 +15,22 @@ namespace DavideTonelli
         private const float DifficultyFactor = 1.010f;
         private const float MaxDifficulty = 15.0f;
 
-        private IMarkerManager markerManager;
-        private float difficulty;
-        private int distance;
+        private IMarkerManager _markerManager;
+        private float _difficulty;
+        private int _distance;
 
         [SetUp]
         public void Setup()
         {
-            markerManager = new MarkerManagerImpl(LastDeathDistance, RecordDistance);
-            difficulty = 2;
-            distance = 0;
+            _markerManager = new MarkerManagerImpl(LastDeathDistance, RecordDistance);
+            _difficulty = 2;
+            _distance = 0;
         }
 
         [Test]
         public void InitialState()
         {
-            Assert.AreEqual(0, markerManager.GetMarkers().Count);
+            Assert.AreEqual(0, _markerManager.GetMarkers().Count);
         }
 
         [Test]
@@ -41,39 +41,39 @@ namespace DavideTonelli
             string markerString = "";
             while (tempDistance <= DistanceBetweenMarkers)
             {
-                if (markerManager.IsCommonMarkerToBeCreated(tempDistance))
+                if (_markerManager.IsCommonMarkerToBeCreated(tempDistance))
                 {
                     // Creates a common marker
-                    markerManager.GetMarkers().Add(Optional.Of(factory.CreateCommonMarker(DistanceBetweenMarkers.ToString())));
+                    _markerManager.GetMarkers().Add(Optional.Of(factory.CreateCommonMarker(DistanceBetweenMarkers.ToString())));
                     // Saves the string distance of the common marker
                     markerString = tempDistance.ToString();
                 }
                 IncreaseDifficulty();
                 IncreaseDistance();
-                tempDistance = distance;
+                tempDistance = _distance;
             }
-            Assert.AreEqual(1, markerManager.GetMarkers().Count);
-            Assert.AreEqual(markerString, markerManager.GetMarkers()[0].GetValueOrElseThrow().Text);
+            Assert.AreEqual(1, _markerManager.GetMarkers().Count);
+            Assert.AreEqual(markerString, _markerManager.GetMarkers()[0].GetValueOrElseThrow().Text);
             // Moves markers out of the screen
-            markerManager.Update(BigDistance);
-            Assert.AreEqual(0, markerManager.GetMarkers().Count);
+            _markerManager.Update(BigDistance);
+            Assert.AreEqual(0, _markerManager.GetMarkers().Count);
         }
 
         [Test]
         public void TestLastDeathMarker()
         {
             int tempDistance = 0;
-            while (distance <= LastDeathDistance * 2)
+            while (_distance <= LastDeathDistance * 2)
             {
-                markerManager.CheckCreateMarker(tempDistance);
+                _markerManager.CheckCreateMarker(tempDistance);
                 IncreaseDifficulty();
                 IncreaseDistance();
-                tempDistance = distance;
+                tempDistance = _distance;
             }
-            Assert.AreEqual(NumberOfMarkersLastDeathTest, markerManager.GetMarkers().Count);
+            Assert.AreEqual(NumberOfMarkersLastDeathTest, _markerManager.GetMarkers().Count);
             // Moves markers out of the screen
-            markerManager.Update(BigDistance);
-            Assert.AreEqual(0, markerManager.GetMarkers().Count);
+            _markerManager.Update(BigDistance);
+            Assert.AreEqual(0, _markerManager.GetMarkers().Count);
         }
 
         [Test]
@@ -82,21 +82,21 @@ namespace DavideTonelli
             int tempDistance = 0;
             while (tempDistance <= RecordDistance * 2)
             {
-                markerManager.CheckCreateMarker(tempDistance);
+                _markerManager.CheckCreateMarker(tempDistance);
                 IncreaseDifficulty();
                 IncreaseDistance();
-                tempDistance = distance;
+                tempDistance = _distance;
             }
-            Assert.AreEqual(NumberOfMarkersRecordTest, markerManager.GetMarkers().Count);
+            Assert.AreEqual(NumberOfMarkersRecordTest, _markerManager.GetMarkers().Count);
             // Moves markers out of the screen
-            markerManager.Update(BigDistance);
-            Assert.AreEqual(0, markerManager.GetMarkers().Count);
+            _markerManager.Update(BigDistance);
+            Assert.AreEqual(0, _markerManager.GetMarkers().Count);
         }
 
         private void IncreaseDifficulty() => 
-            difficulty = difficulty <= MaxDifficulty ? difficulty * DifficultyFactor : difficulty;
+            _difficulty = _difficulty <= MaxDifficulty ? _difficulty * DifficultyFactor : _difficulty;
 
-        private void IncreaseDistance() => distance += (int)difficulty;
+        private void IncreaseDistance() => _distance += (int)_difficulty;
 
     }
 }
