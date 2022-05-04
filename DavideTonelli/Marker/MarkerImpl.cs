478343dab66d049b7a716bb2ebf94f5316b05b26
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-05-04 16:29:41 +0200

    Add MarkerImpl implemented

---
+++ DavideTonelli/Marker/MarkerImpl.cs
@@ -0,0 +1,38 @@
+﻿using Commons.Geometry;
+
+namespace DavideTonelli.Marker
+{
+    /// <summary>
+    /// A simple marker.
+    /// Implements marker.
+    /// </summary>
+    class MarkerImpl : IMarker
+    {
+        /// <summary>
+        /// Creates a new MarkerImpl.
+        /// </summary>
+        /// <param name="coordinates">The position of the marker.</param>
+        /// <param name="image">The image of the marker.</param>
+        /// <param name="text">The notice of the marker.</param>
+        public MarkerImpl(Point coordinates, Image image, string text)
+        {
+            Coordinates = coordinates;
+            Image = image;
+            Dimension = new Size(Image.Width, Image.Height);
+            Text = text;
+        }
+
+        public Point Coordinates { get; }
+
+        public Image Image { get; }
+
+        public Size Dimension { get; }
+
+        public string Text { get; }
+
+        public bool IsOutOfScreen() => Coordinates.X < -Dimension.Width;
+
+        public void Update(float velocity) => Coordinates.X -= velocity;
+
+    }
+}
