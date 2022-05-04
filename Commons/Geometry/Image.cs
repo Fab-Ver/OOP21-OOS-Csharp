bb0afa169ab3d34ff913e8d556175cf0f65cc620
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-05-04 15:52:50 +0200

    Add Image class

---
+++ Commons/Geometry/Image.cs
@@ -0,0 +1,22 @@
+﻿namespace Commons.Geometry
+{
+    /// <summary>
+    /// Class used to store an image. 
+    /// </summary>
+    public sealed class Image
+    {
+        private const float testDim = 50.0f;
+
+        /// <summary>
+        /// Create a new Image
+        /// </summary>
+        public Image()
+        {
+            Width = testDim;
+            Height = testDim;
+        }
+
+        public float Width { get; }
+        public float Height { get; }
+    }
+}
